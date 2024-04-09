c5df6cbcbe4869ff47cf2adaa3a77f999c75f4ed
il <>
2024-04-10 00:34:50 +0300

    add reflection

---
+++ src/main/java/ru/reflection/example/Main.java
@@ -0,0 +1,15 @@
+package ru.reflection.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
